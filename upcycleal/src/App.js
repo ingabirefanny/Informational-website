de47773c17c698b12f772efa161250ab46b4bed7
Faith-yiamat <oloserianyiamat@gmail.com>
2024-07-02 19:00:25 +0300

    add about us

--- upcycleal/src/App.js
+++ upcycleal/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Team from './Team';
+import About from './AboutUs'
 
 function App() {
   return (
     <div className="App">
-<Team/> 
+      <About/>
+      <Team/> 
     </div>
   );
 }
