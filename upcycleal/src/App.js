9339874120eb3f3254af3fb79542d925dff55a2a
ingabirefanny <fannyingabiree@gmail.com>
2024-07-24 10:34:23 +0300

    add components

--- upcycleal/src/App.js
+++ upcycleal/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import Team from './Team';
 import About from './AboutUs'
 import Roadmap from './Roadmap'
+import Navbar from './Navbar';
+import Homepage from './Homepage'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Homepage/>
       <About/>
       <Roadmap/>
       <Team/> 
